c3213e36665cb567424f4c5d6c664d6411ad16ae
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-04-21 15:11:08 +0300

    ✨ feat(app): focusing searchbar with 'Enter' key

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useRef } from 'react';
+
 const SearchField = ({ searchHandler }) => {
+  const inputDOM = useRef(null);
+
+  useEffect(() => {
+    const callback = (e) => {
+      if (e.code !== 'Enter') return;
+
+      if (inputDOM.current === document.activeElement) return;
+
+      inputDOM.current.focus();
+
+      inputDOM.current.value = '';
+      searchHandler('');
+    };
+
+    document.addEventListener('keydown', callback);
+
+    return () => document.removeEventListener('keydown', callback);
+  }, [searchHandler]);
+
   return (
     <input
+      ref={inputDOM}
       className="search"
       type="text"
       placeholder="Search movies..."
